5b4fb1153889efc75d7255521167adb911f79c77
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-11-06 13:59:59 +0700

    enhance(Pickup): comment update saldo pinjaman

--- app/Http/Controllers/AcctNominativeSavingsPickupController.php
+++ app/Http/Controllers/AcctNominativeSavingsPickupController.php
@@ -277,15 +277,16 @@ class AcctNominativeSavingsPickupController extends Controller
             ->first()
             ->transaction_module_id;
 
-            $acctcreditsaccount = AcctCreditsAccount::findOrFail($data['credits_account_id']);
-            $acctcreditsaccount->credits_account_last_balance           = $data['credits_principal_last_balance'];
-            $acctcreditsaccount->credits_account_last_payment_date      = $data['credits_payment_date'];
-            $acctcreditsaccount->credits_account_interest_last_balance  = $data['credits_interest_last_balance'];
-            $acctcreditsaccount->credits_account_payment_date           = $credits_account_payment_date;
-            $acctcreditsaccount->credits_account_payment_to             = $data['credits_payment_to'];
-            $acctcreditsaccount->credits_account_accumulated_fines      = $request->credits_account_accumulated_fines;
-            $acctcreditsaccount->credits_account_status                 = $credits_account_status;
-            $acctcreditsaccount->save();
+            //update saldo pinjaman
+            // $acctcreditsaccount = AcctCreditsAccount::findOrFail($data['credits_account_id']);
+            // $acctcreditsaccount->credits_account_last_balance           = $data['credits_principal_last_balance'];
+            // $acctcreditsaccount->credits_account_last_payment_date      = $data['credits_payment_date'];
+            // $acctcreditsaccount->credits_account_interest_last_balance  = $data['credits_interest_last_balance'];
+            // $acctcreditsaccount->credits_account_payment_date           = $credits_account_payment_date;
+            // $acctcreditsaccount->credits_account_payment_to             = $data['credits_payment_to'];
+            // $acctcreditsaccount->credits_account_accumulated_fines      = $request->credits_account_accumulated_fines;
+            // $acctcreditsaccount->credits_account_status                 = $credits_account_status;
+            // $acctcreditsaccount->save();
 
             if($request->member_mandatory_savings > 0 && $request->member_mandatory_savings != ''){
                 $data_detail = array (
